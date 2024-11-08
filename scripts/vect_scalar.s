bd304aa219eb4a3fba021184a25571fe93edeef6
Emanuele <eman_93@hotmail.it>
2024-11-08 21:59:53 +0100

    fix: typo line 10 and line 14

--- scripts/vect_scalar.s
+++ scripts/vect_scalar.s
@@ -7,11 +7,11 @@ vact_scalar:
         mov r3, #0 // i = 0 - indice loop
     loop:cmp r3, r2 // i == size
         beq end // se uguali salta a fine
-        ldr r12, [r0, r3, lsl #2] // carica l'array
+        ldr r12, [r0, r3 lsl #2] // carica l'array
         mul r12, r12, #r1 // moltiplica array[i] con lo scalare passato alla funzione
                         // che è in r2
 
-        str r12, [r0, r3, lsl #2] // carica il nuovo elemento
+        str r12, [r0, r3 lsl #2] // carica il nuovo elemento
         add r3, r3, #1 // i++
         b loop
     fine:
