2d6fcb95fe804eff81039a6a8c5dc414067ebc06
maxcohen31 <eman_93@hotmail.it>
2025-07-15 11:23:24 +0200

    fix: comments

--- scripts/check_list.s
+++ scripts/check_list.s
@@ -2,45 +2,40 @@
     .global addDistinct
     .type addDistinct, %function
 
-/* r0 -> pointer l1 */
-/* r1 -> pointer l2 */
-
 addDistinct:
-    cmp r0, #0;
+    cmp r0, #0             @ se L1 (r0) == NULL, salta alla fine
     beq _end    
-    cmp r1, #0
+    cmp r1, #0             @ se L2 (r1) == NULL, salta alla fine
     beq _end
     
-    push {r4-r7, lr}
-    mov r4, r0 @ r4 = l1
-    mov r5, r1 @ r5 = l2
+    push {r4-r7, lr}       
+    mov r4, r0             @ r4 = L1
+    mov r5, r1             @ r5 = L2
 
 _loop:
-    cmp r5, #0 @ l2 == NULL
+    cmp r5, #0             @ se L2 == NULL, esco dal ciclo
     beq _end_loop
-    ldr r6, [r5] @ load l2->value
-    push {r0-r3}
-    mov r0, r4 @ first parameter 
-    mov r1, r6 @ second parameter
-    bl check
-    mov r12, r0
-    pop {r0-r3}
-    cmp r12, #0
-    beq _add
-    ldr r5, [r5, #4] @ l2 = l2->next
+    
+    ldr r6, [r5]           @ r6 = valore del nodo corrente L2
+    push {r0-r3}           @ salvo i registri che verranno modificati
+    mov r0, r4             @ primo parametro: puntatore alla lista L1
+    mov r1, r6             @ secondo parametro: valore da cercare in L1
+    bl check               @ chiamata a check(L1, valore)
+    mov r12, r0            @ salvo il risultato in un registro sicuro
+    pop {r0-r3}            @ ripristino r0-r3 originali 
+    cmp r12, #0            @ se check restituisce 0 (non trovato), aggiungo
+    beq _add               
+    ldr r5, [r5, #4]       @ L2 = L2->next
     b _loop
-_add: 
-    mov r7, r5 @ save current node
-    ldr r5, [r5, #4] @ l2 = l2->next
-    str r4, [r7, #4] @ node->next = l1
-    mov r4, r7 @ l1 = node
+_add:
+    mov r7, r5             @ r7 = nodo corrente da aggiungere -> puntatore a L2->value
+    ldr r5, [r5, #4]       @ avanzo L2 PRIMA di modificare `next`
+    str r4, [r7, #4]       @ nodo->next = vecchia testa L1. str r4, [r7] scriverebbe un puntatore nel campo value di L1 
+    mov r4, r7             @ aggiorno L1: nuovo nodo in testa
     b _loop
-_end:
-    mov pc, lr
 _end_loop:
-    mov r0, r4
-    pop {r4-r7, lr}
-    mov pc, lr
-
-
+    mov r0, r4             
+    pop {r4-r7, lr}        
+_end:
+    mov pc, lr             
 
