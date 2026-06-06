4f940aabfbb4b2f16fea6fdb000029eb72db4290
maxcohen31 <eman_93@hotmail.it>
2026-06-06 16:14:19 +0200

    more readable code

--- scripts/esponenziale_ric.s
+++ scripts/esponenziale_ric.s
@@ -2,27 +2,22 @@
 .global rec_exp
 .type rec_exp, %function
 
-@ r0: base
-@ r1: potenza
+/* Calcola la potenza di un numero ricorsivamente */
+/* r0 = base; r1 = esp */
 
 rec_exp:
+    /* caso base */
     cmp r1, #0
-    beq _return_1
-    cmp r1, #1
-    beq _return_n
+    beq _return
 
-    @ ricorsione
-    sub r1, r1, #1 @ pow - 1
-    push {r0, r1, lr}
-    bl rec_exp
-    mov r2, r0 @ salva il risultato in r2
-    pop {r0, r1, lr}
-    mul r0, r0, r2
-    mov pc, lr
-
-_return_1:
-    mov r0, #1
-    mov pc, lr
-_return_n:
+    /* ricorsione: base * rec_exp(base, pow - 1) */
+    sub r1, r1, #1 /* r1 = pow - 1 */
+    push {r0, r1, lr} /* salvo r0, r1, lr */
+    bl rec_exp /* chiama rec_exp(base, pow - 1). Risultato in r0 */
+    pop {r2, r3, lr} /* ripristina base in r2 e pow - 1 in r3 */
+    mul r0, r0, r2 /* salvo in r0 il risultato di base * rec_exp(base, pow - 1) */
     mov pc, lr
     
+_return:
+    mov r0, #1
+    mov pc, lr
