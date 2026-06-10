93317451d135c1dba9d1027c1bf890a83ea98f80
maxcohen31 <eman_93@hotmail.it>
2026-06-10 14:42:44 +0200

    fix: remove r3

--- scripts/higher_order_string.s
+++ scripts/higher_order_string.s
@@ -11,16 +11,15 @@ conta_vocali:
     beq _exit
     mov r4, r0 // salvo il puntatore alla stringa
     mov r2, #0 // risultato
-    mov r3, #0 // i = 0
 _loop:
-    ldrb r12, [r4], #1 // carica str[i]
+    ldrb r12, [r4], #1 // carica str[i] e incrementa di 1 byte il puntatore
     cmp r12, #0 // str == 0
     beq _return 
-    push {r1, r2} // registri non preservati - salviamoli
+    push {r1, r2} // r1 e r2 sono registri non preservati che dovrò riutilizzare. Salviamoli
     mov r0, r12 // passo il puntatore alla stringa alla funzione f
     blx r1 // chiamo f 
     pop {r1, r2}
-    add r2, r2, r0
+    add r2, r2, r0 // accumulo il risultato
     b _loop
 _return:
     mov r0, r2
